c558409401c0fa95adddb7790e2adb0e9b9dc22e
Zheng Peng <pengzheng1992@163.com>
2019-07-25 18:14:02 +0800

    handle no args case

--- DIAL/experiment/DistributedCounter/DIAL-v3 - random place/parse_args.cpp
+++ DIAL/experiment/DistributedCounter/DIAL-v3 - random place/parse_args.cpp
@@ -4,10 +4,17 @@
 #include "common.h"
 
 void print_usage() {
-    fprintf(stderr, "Usage: ./pcap_offline -p pcap_filename -t topo_filename\ntopofile format: node_number, edge_number then source destination pair for each edge\n");
+    fprintf(stderr, "Usage: ./dial -p <pcap_filename> -t <topo_filename>\ntopofile format: node_number, edge_number then source destination pair for each edge\n");
+    fprintf(stderr, "Easiest usage: make run\n");
 }
 
 int parse_args(IN int argc, IN char **argv, OUT const char **pcap_file, OUT const char **topo_file) {
+    fprintf(stdout, "argc: %d\n", argc);
+	if (argc <= 1) {
+            fprintf(stderr, "Too few arguments.\n");
+            print_usage();
+            return 1;
+	}
     for (int i = 1; i < argc; ++i) {
         char *arg = argv[i];
         if (!strcmp(arg, "-p") && i + 1 < argc)	{
